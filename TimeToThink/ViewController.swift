1938f678667332010b2a3fd88ef0effb48378ca7
Konstantin Savonevich <kossav@icloud.com>
2019-05-05 10:02:35 +0300

    Initial Commit

---
+++ TimeToThink/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeToThink
+//
+//  Created by Savonevich Constantine on 5/5/19.
+//  Copyright © 2019 Savonevich Konstantin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
